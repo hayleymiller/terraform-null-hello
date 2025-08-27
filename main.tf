841c50656c80eda4499d7429b17ce215e8c3e073
hayleymiller <hayley.miller@ibm.com>
2025-08-27 00:33:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-# terraform-null-hello/main.tf
 resource "random_pet" "name" {
   prefix = var.name_prefix
 }
 
 resource "null_resource" "hello" {
   triggers = {
-    # cost_center = var.cost_center   # uncomment when you want policy to PASS
-    name  = random_pet.name.id
+    cost_center = var.cost_center
+    name        = random_pet.name.id
   }
 }
