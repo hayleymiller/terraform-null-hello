de93c5b7b16706ebbda90b78d8f40c2cd68d68c0
hayleymiller <hayley.miller@ibm.com>
2025-08-26 23:56:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ resource "random_pet" "name" {
   prefix = var.name_prefix
 }
 
-# Safe local resource we can 'tag' via triggers
-resource "null_resource" "hey there" {
+resource "null_resource" "hello" {
   triggers = {
     #cost_center = var.cost_center
     name        = random_pet.name.id
+    stamp = var.stamp
   }
 }
