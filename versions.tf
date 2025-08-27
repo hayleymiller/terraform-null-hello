2ca71e282b7f98ed90c1834941bbeb511105f0cf
hayleymiller <hayley.miller@ibm.com>
2025-08-26 22:30:42 -0500

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    null   = { source = "hashicorp/null",  version = ">= 3.2.2" }
+    random = { source = "hashicorp/random", version = ">= 3.6.0" }
+  }
+}
