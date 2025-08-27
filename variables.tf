f4281e300dc795dd75b2067c22071909cf8f92d1
hayleymiller <hayley.miller@ibm.com>
2025-08-27 00:27:15 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,3 @@
-variable "cost_center" {
-  type        = string
-  default = "acme-RND"
-  description = "Required business tag / cost allocation"
-}
-
-variable "name_prefix" {
-  type        = string
-  description = "Prefix used"
-  default     = "acme"
-}
-
-# NEW: toggle to force updates to null_resource
-#variable "stamp" {
-  #type    = string
-  #default = "v1"
-#}
-
+# terraform-null-hello/variables.tf
+variable "name_prefix" { type = string }
+variable "cost_center" { type = string }
