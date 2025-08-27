ba9b29dccede16a0f16a03a92c346933cc327920
hayleymiller <hayley.miller@ibm.com>
2025-08-27 00:26:33 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,13 @@
+# terraform-null-hello/versions.tf
 terraform {
-  required_version = ">= 1.6.0"
   required_providers {
-    null   = { source = "hashicorp/null",  version = ">= 3.2.2" }
-    random = { source = "hashicorp/random", version = ">= 3.6.0" }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
   }
 }
