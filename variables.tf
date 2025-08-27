f1cff63e053e2c0fcbecf31e3cb4055bfba70c5e
hayleymiller <hayley.miller@ibm.com>
2025-08-26 23:55:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,18 @@
 variable "cost_center" {
   type        = string
+  default = "acme-RND"
   description = "Required business tag / cost allocation"
 }
 
 variable "name_prefix" {
   type        = string
-  description = "Prefix used to generate a friendly name"
+  description = "Prefix used"
   default     = "acme"
 }
+
+# NEW: toggle to force updates to null_resource
+variable "stamp" {
+  type    = string
+  default = "v1"
+}
+
